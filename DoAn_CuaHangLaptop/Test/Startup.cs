36d0669b2faeaa6502915ce31a69ca5af3e1dd1d
ducvuive <duc20062001@gmail.com>
2022-08-04 13:48:52 +0700

    d
    d

--- DoAn_CuaHangLaptop/Test/Startup.cs
+++ DoAn_CuaHangLaptop/Test/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using Test.Data;
